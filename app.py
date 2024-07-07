492474812dcf75832f82b4d6d028de19c76deeb5
im-varun <vrun.mulchandani@gmail.com>
2024-07-07 15:03:27 -0500

    Set page configurations

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 from category_mapping import arxiv_mapping
 
+st.set_page_config('Daily Research', ':newspaper:')
+
 category = st.selectbox('Research Field', arxiv_mapping.keys(), index=None, placeholder='Select a Research Field')
