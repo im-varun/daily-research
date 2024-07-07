1e69995ac35b04b229241c37b906d7f65584fa0f
im-varun <vrun.mulchandani@gmail.com>
2024-07-07 14:21:35 -0500

    Return feed entries from arxiv_research()

--- research.py
+++ research.py
@@ -3,12 +3,11 @@ from category_mapping import arxiv_mapping
 
 def arxiv_research(category):
     category_abbreviation = arxiv_mapping.get(category)
-
     url = f'https://rss.arxiv.org/rss/{category_abbreviation}'
 
     feed = feedparser.parse(url)
 
-    print(feed)
+    return feed.get('entries')
 
 if __name__ == "__main__":
     arxiv_research('Computer Science')
