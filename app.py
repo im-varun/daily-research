c3c3d65edc4dd06c46b5d678e3157c23ef8f6ee1
im-varun <vrun.mulchandani@gmail.com>
2024-07-08 16:11:20 -0500

    Add layout for feed display

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 import streamlit as st
 from category_mapping import arxiv_mapping
+from research import arxiv_research
 
 st.set_page_config('Daily Research', ':newspaper:')
 
 category = st.selectbox('Research Field', arxiv_mapping.keys(), index=None, placeholder='Select a Research Field')
+entries = arxiv_research(category)
+
+for entry in entries:
+    with st.expander(entry.get('title')):
+        st.write('Authors: ', entry.get('author'))
+        
+        arxiv_id = entry.get('id').split(':')[2]
+        st.write('arXiv ID: ', arxiv_id)
+        
+        st.write('Announce Type: ', entry.get('arxiv_announce_type'))
+
+        summary = entry.get('summary')
+
+        abstract_index = summary.index('Abstract')
+        abstract = summary[abstract_index:]
+
+        st.write(abstract)
+
+        arxiv_link = entry.get('link')
+        pdf_link = arxiv_link.replace('abs', 'pdf')
+
+        st.link_button('arXiv', arxiv_link)
+        st.link_button('PDF', pdf_link)
