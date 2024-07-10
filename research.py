2f6ce61545e6e5a89890569b8c550b53f9804ffd
im-varun <vrun.mulchandani@gmail.com>
2024-07-10 13:50:28 -0500

    Remove arxiv mappings to reduce coupling

--- research.py
+++ research.py
@@ -1,8 +1,6 @@
 import feedparser
-from category_mapping import arxiv_mapping
 
-def arxiv_research(category):
-    category_abbreviation = arxiv_mapping.get(category)
+def arxiv_research(category_abbreviation):
     url = f'https://rss.arxiv.org/rss/{category_abbreviation}'
 
     feed = feedparser.parse(url)
