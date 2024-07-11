76528adfcbe95ee2f9a428c676dcb86502e42e14
im-varun <vrun.mulchandani@gmail.com>
2024-07-11 17:37:15 -0500

    Add announce type parameter in arxiv_research()

--- research.py
+++ research.py
@@ -1,9 +1,20 @@
 import feedparser
 
-def arxiv_research(data_endpoint):
+def arxiv_research(data_endpoint, requested_announce_type):
     url = f'https://rss.arxiv.org/rss/{data_endpoint}'
 
     feed = feedparser.parse(url)
     entries = feed.get('entries')
 
-    return entries
+    if requested_announce_type == 'all':
+        return entries
+    else:
+        processed_entries = []
+
+        for entry in entries:
+            announce_type = entry.get('arxiv_announce_type')
+
+            if (announce_type == requested_announce_type):
+                processed_entries.append(entry)
+
+        return processed_entries
