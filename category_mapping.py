44b0d886895fb81c9f96ed18c7beca3f01d0a2d7
im-varun <vrun.mulchandani@gmail.com>
2024-07-10 19:33:47 -0500

    Add subcategories in arxiv mapping and change its structure

--- category_mapping.py
+++ category_mapping.py
@@ -1,22 +1,168 @@
 arxiv_mapping = {
-    'Computer Science': 'cs', 
-    'Economics': 'econ', 
-    'Electrical Engineering and System Science': 'eess', 
-    'Mathematics': 'math', 
-    'Physics': 'physics', 
-    'Astrophysics': 'astro-ph', 
-    'Quantum Physics': 'quant-ph', 
-    'Condensed Matter': 'cond-mat', 
-    'General Relativity and Quantum Cosmology': 'gr-qc', 
-    'High Energy Physics - Experiment': 'hep-ex', 
-    'High Energy Physics - Lattice': 'hep-lat', 
-    'High Energy Physics - Phenomenology': 'hep-ph', 
-    'High Energy Physics - Theory': 'hep-th', 
-    'Mathematical Physics': 'math-ph', 
-    'Nonlinear Sciences': 'nlin', 
-    'Nuclear Experiment': 'nucl-ex', 
-    'Nuclear Theory': 'nucl-th', 
-    'Quantitative Biology': 'q-bio', 
-    'Quantitative Finance': 'q-fin', 
-    'Statistics': 'stat'
+    'Computer Science': {
+        'category_abbreviation': 'cs', 
+        'sub_categories': {
+            'Artificial Intelligence': 'AI', 'Hardware Architecture': 'AR', 
+            'Computational Complexity': 'CC', 'Computational Engineering, Finance, and Science': 'CE', 
+            'Computational Geometry': 'CG', 'Computation and Language': 'CL', 
+            'Cryptography and Security': 'CR', 'Computer Vision and Pattern Recognition': 'CV', 
+            'Computers and Society': 'CY', 'Databases': 'DB', 
+            'Distributed, Parallel, and Cluster Computing': 'DC', 'Digital Libraries': 'DL', 
+            'Discrete Mathematics': 'DM', 'Data Structures and Algorithms': 'DS', 
+            'Emerging Technologies': 'ET', 'Formal Languages and Automata Theory': 'FL', 
+            'General Literature': 'GL', 'Graphics': 'GR', 
+            'Computer Science and Game Theory': 'GT', 'Human-Computer Interaction': 'HC', 
+            'Information Retrieval': 'IR', 'Information Theory': 'IT', 
+            'Machine Learning': 'LG', 'Logic in Computer Science': 'LO', 
+            'Multiagent Systems': 'MA', 'Multimedia': 'MM', 
+            'Mathematical Software': 'MS', 'Numerical Analysis': 'NA', 
+            'Neural and Evolutionary Computing': 'NE', 'Networking and Internet Architecture': 'NI', 
+            'Other Computer Science': 'OH', 'Operating Systems': 'OS', 
+            'Performance': 'PF', 'Programming Languages': 'PL', 
+            'Robotics': 'RO', 'Symbolic Computation': 'SC', 
+            'Sound': 'SD', 'Software Engineering': 'SE', 
+            'Social and Information Networks': 'SI', 'Systems and Control': 'SY'
+            }
+        }, 
+    'Economics': {
+        'category_abbreviation': 'econ', 
+        'sub_categories': {
+            'Econometrics': 'EM', 'General Economics': 'GN', 
+            'Theoretical Economics': 'TH'
+            }
+        }, 
+    'Electrical Engineering and System Science': {
+        'category_abbreviation': 'eess', 
+        'sub_categories': {
+            'Audio and Speech Processing': 'AS', 'Image and Video Processing': 'IV', 
+            'Signal Processing': 'SP', 'Systems and Control': 'SY'
+            }
+        }, 
+    'Mathematics': {
+        'category_abbreviation': 'math', 
+        'sub_categories': {
+            'Commutative Algebra': 'AC', 'Algebraic Geometry': 'AG', 
+            'Analysis of PDEs': 'AP', 'Algebraic Topology': 'AT', 
+            'Classical Analysis and ODEs': 'CA', 'Combinatorics': 'CO', 
+            'Category Theory': 'CT', 'Complex Variables': 'CV', 
+            'Differential Geometry': 'DG', 'Dynamical Systems': 'DS', 
+            'Functional Analysis': 'FA', 'General Mathematics': 'GM', 
+            'General Topology': 'GN', 'Group Theory': 'GR', 
+            'Geometric Topology': 'GT', 'History and Overview': 'HO', 
+            'Information Theory': 'IT', 'K-Theory and Homology': 'KT', 
+            'Logic': 'LO', 'Metric Geometry': 'MG', 
+            'Mathematical Physics': 'MP', 'Numerical Analysis': 'NA', 
+            'Number Theory': 'NT', 'Operator Algebras': 'OA', 
+            'Optimization and Control': 'OC', 'Probability': 'PR', 
+            'Quantum Algebra': 'QA', 'Rings and Algebras': 'RA', 
+            'Representation Theory': 'RT', 'Symplectic Geometry': 'SG', 
+            'Spectral Theory': 'SP', 'Statistics Theory': 'ST'
+            }
+        }, 
+    'Physics': {
+        'category_abbreviation': 'physics', 
+        'sub_categories': {
+            'Accelerator Physics': 'acc-ph', 'Atmospheric and Oceanic Physics': 'ao-ph', 
+            'Applied Physics': 'app-ph', 'Atomic and Molecular Clusters': 'atm-clus', 
+            'Atomic Physics': 'atom-ph', 'Biological Physics': 'bio-ph', 
+            'Chemical Physics': 'chem-ph', 'Classical Physics': 'class-ph', 
+            'Computational Physics': 'comp-ph', 'Data Analysis, Statistics and Probability': 'data-an', 
+            'Physics Education': 'ed-ph', 'Fluid Dynamics': 'flu-dyn', 
+            'General Physics': 'gen-ph', 'Geophysics': 'geo-ph', 
+            'History and Philosophy of Physics': 'hist-ph', 'Instrumentation and Detectors': 'ins-det', 
+            'Medical Physics': 'med-ph', 'Optics': 'optics', 
+            'Plasma Physics': 'plasm-ph', 'Popular Physics': 'pop-ph', 
+            'Physics and Society': 'soc-ph', 'Space Physics': 'space-ph'
+            }
+        }, 
+    'Astrophysics': {
+        'category_abbreviation': 'astro-ph', 
+        'sub_categories': {
+            'Cosmology and Nongalactic Astrophysics': 'CO', 'Earth and Planetary Astrophysics': 'EP', 
+            'Astrophysics of Galaxies': 'GA', 'High Energy Astrophysical Phenomena': 'HE', 
+            'Instrumentation and Methods for Astrophysics': 'IM', 'Solar and Stellar Astrophysics': 'SR'
+            }
+        }, 
+    'Quantum Physics': {
+        'category_abbreviation': 'quant-ph', 
+        'sub_categories': {}
+        }, 
+    'Condensed Matter': {
+        'category_abbreviation': 'cond-mat', 
+        'sub_categories': {
+            'Disordered Systems and Neural Networks': 'dis-nn', 'Mesoscale and Nanoscale Physics': 'mes-hall', 
+            'Materials Science': 'mtrl-sci', 'Other Condensed Matter': 'other', 
+            'Quantum Gases': 'quant-gas', 'Soft Condensed Matter': 'soft', 
+            'Statistical Mechanics': 'stat-mech', 'Strongly Correlated Electrons': 'str-el', 
+            'Superconductivity': 'supr-con'
+            }
+        }, 
+    'General Relativity and Quantum Cosmology': {
+        'category_abbreviation': 'gr-qc', 
+        'sub_categories': {}
+        }, 
+    'High Energy Physics - Experiment': {
+        'category_abbreviation': 'hep-ex', 
+        'sub_categories': {}
+        }, 
+    'High Energy Physics - Lattice': {
+        'category_abbreviation': 'hep-lat', 
+        'sub_categories': {}
+        }, 
+    'High Energy Physics - Phenomenology': {
+        'category_abbreviation': 'hep-ph', 
+        'sub_categories': {}
+        }, 
+    'High Energy Physics - Theory': {
+        'category_abbreviation': 'hep-th', 
+        'sub_categories': {}
+        }, 
+    'Mathematical Physics': {
+        'category_abbreviation': 'math-ph', 
+        'sub_categories': {}
+        }, 
+    'Nonlinear Sciences': {
+        'category_abbreviation': 'nlin', 
+        'sub_categories': {
+            'Adaptation and Self-Organizing Systems': 'AO', 'Chaotic Dynamics': 'CD', 
+            'Cellular Automata and Lattice Gases': 'CG', 'Pattern Formation and Solitons': 'PS', 
+            'Exactly Solvable and Integrable Systems': 'SI'
+            }
+        }, 
+    'Nuclear Experiment': {
+        'category_abbreviation': 'nucl-ex', 
+        'sub_categories': {}
+        }, 
+    'Nuclear Theory': {
+        'category_abbreviation': 'nucl-th', 
+        'sub_categories': {}
+        }, 
+    'Quantitative Biology': {
+        'category_abbreviation': 'q-bio', 
+        'sub_categories': {
+            'Biomolecules': 'BM', 'Cell Behavior': 'CB', 
+            'Genomics': 'GN', 'Molecular Networks': 'MN', 
+            'Neurons and Cognition': 'NC', 'Other Quantitative Biology': 'OT', 
+            'Populations and Evolution': 'PE', 'Quantitative Methods': 'QM', 
+            'Subcellular Processes': 'SC', 'Tissues and Organs': 'TO'
+            }
+        }, 
+    'Quantitative Finance': {
+        'category_abbreviation': 'q-fin', 
+        'sub_categories': {
+            'Computational Finance': 'CP', 'Economics': 'EC', 
+            'General Finance': 'GN', 'Mathematical Finance': 'MF', 
+            'Portfolio Management': 'PM', 'Pricing of Securities': 'PR', 
+            'Risk Management': 'RM', 'Statistical Finance': 'ST', 
+            'Trading and Market Microstructure': 'TR'
+            }
+        }, 
+    'Statistics': {
+        'category_abbreviation': 'stat', 
+        'sub_categories': {
+            'Applications': 'AP', 'Computation': 'CO', 
+            'Methodology': 'ME', 'Machine Learning': 'ML', 
+            'Other Statistics': 'OT', 'Statistics Theory': 'TH'
+            }
+        }
     }
